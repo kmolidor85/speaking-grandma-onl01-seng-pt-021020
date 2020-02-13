e3e1c2b47ba54c2c640445e749685455efff0d23
kyle molidor <k_molidor@yahoo.com>
2020-02-13 06:49:05 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -6,7 +6,7 @@ def speak_to_grandma(phrase)
   	return "HUH?! SPEAK UP, SONNY!"
   elsif phrase == phrase.downcase
       return "HUH?! SPEAK UP, SONNY!"
-  elsif phrase == phrase.upcase
+  else phrase == phrase.upcase
     return "NO, NOT SINCE 1938!"
     
 end
